931f16499791604fce2f5ea24d03a69e979fdf3d
Emil Asplund <emilasplund86@gmail.com>
2017-02-27 21:35:32 +0100

    Lagt till "sv_SE.UTF-8" i setlocale.

--- ovningar/9/9_8/main.c
+++ ovningar/9/9_8/main.c
@@ -14,7 +14,7 @@ int index_of_backwards(const wchar_t *s, wchar_t c){
 
 int main(){ 
 
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "sv_SE.UTF-8");
     
 
     wchar_t string[] = L"Hejsan på dig!";
