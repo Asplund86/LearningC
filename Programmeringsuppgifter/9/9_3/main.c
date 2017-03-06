7086892eef7f973d53398d684a2c2793ea102c3c
Asplund86 <emilasplund86@gmail.com>
2017-03-06 23:00:04 +0100

    Installerar Git på Laptop

--- Programmeringsuppgifter/9/9_3/main.c
+++ Programmeringsuppgifter/9/9_3/main.c
@@ -1,47 +1,73 @@
 #include <stdio.h>
-#include <wctype.h>
 #include <wchar.h>
+#include <wctype.h>
 #include <locale.h>
+#include <stdlib.h>
+
+_Bool checkArrayforKons(wchar_t *arr){
+	wchar_t kons[] = L"aouåeiyäöAOUÅEIYÄÖ";
+	for(int i = 0; arr[i] != '\0'; i++){
+		int j = 0;
+		for(; j < wcslen(kons)-1; j++){
+			if(arr[i] == kons[j])
+				return 1;
+		}
+	}
+
+	return 0;
+}
+
+_Bool checkElementsForKons(wchar_t x){
+	wchar_t kons[] = L"aouåeiyäöAOUÅEIYÄÖ";
+
+	for(int i = 0; i<wcslen(kons)-1; i++){
+		if(x == kons[i])
+			return 1;
+	}
+	return 0;
+}
 
 
-//void p(wchar_t *array){
- //   wchar_t kons[] = L"bcdfghjklmnqrstvzx";
-    
 
-   // for(int i = 0; i < wcslen(array); i++){
-     //   for(int j = 0; j<wcslen(kons); j++){
-       //     if(array[i] != kons[j] || array[i] != towupper(kons[j]))
-             //   printf("%lcp%lc", array[i], array[i]);
-        //}
-        //printf("%lc", array[i]);
-    //}
+void plang(wchar_t *to, wchar_t *from){
 
-//}
+	int tocounter = 0;
+	for(int i = 0; from[i] != '\0'; i++){
+		if(checkElementsForKons(from[i])){
+			to[tocounter++] = from[i];
+			//tocounter++;
+			to[tocounter++] = 'p';
+			//ocounter++;
+			to[tocounter++] = from[i];
+			//tocounter++;
+		}
+		else{
+			to[tocounter++] = from[i];
+		}
+	}
+
+	to[tocounter] = '\0';
+}
+
 
 int main(){
-    wchar_t arr[40];
-    wchar_t kons[] = L"bcdfghjklmnqrstvzx";
+	system("chcp 1252");
+	setlocale(LC_ALL, "sv_SE.UTF-8");
+
+	wchar_t one[100];
+	wchar_t two[100];
+
+	printf("Skriv en mening: \n");
+	fgetws(one, 100, stdin);
+
+	if(checkArrayforKons(one)){
+		plang(two, one);
+
+		printf("%ls", two);
+	}
+	else
+		printf("Innehåller ingen konsonant\n");
 
 
-    printf("Skriv en kort mening: ");
-    scanf(" %ls", arr);
 
-    printf("Du skrev %ls det blir på p-språket ", arr);
-
-    for(int i = 0; i < wcslen(arr); i++){
-        for(int j = 0; j<wcslen(kons); j++){
-            if(arr[i] == kons[j]){
-                printf("%lc", arr[i]);
-            }
-            else if(arr[i] == towupper(kons[j]))
-                printf("%lc", arr[i]);
-
-            else{
-                printf("%lcp%lc", arr[i], arr[i]);
-                j = wcslen(kons);
-            }
-        }
-    }
-    printf("%lc", kons[3]);
 }
- 
