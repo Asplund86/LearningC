7ff0b095b1f3af18d664d058298dd757da2dd3e0
Emil Asplund <emilasplund86@gmail.com>
2017-03-10 10:20:09 +0100

    Lagt till pekare till elib.h

--- ovningar/10/10_1/elib.h
+++ ovningar/10/10_1/elib.h
@@ -2,14 +2,14 @@
 #include <wchar.h>
 #include <wctype.h>
 
-void fskip_line(FILE);
+void fskip_line(FILE *f);
 void skip_line(void);
-_Bool setLineToBreak(wchar_t);
+_Bool setLineToBreak(wchar_t *a);
 _Bool setLineToBreak_char(char *a);
-_Bool fget_line(wchar_t, int , FILE);
+_Bool fget_line(wchar_t *a, int n, FILE);
 _Bool fget_line_char(char *a, int n, FILE *f);
-_Bool get_line(wchar_t, int);
+_Bool get_line(wchar_t *a, int n);
 _Bool get_line_char(char *a, int n);
-FILE *read_file(const wchar_t );
-FILE *writeto_file(const wchar_t);
+FILE *read_file(const wchar_t *commit);
+FILE *writeto_file(const wchar_t *commmit);
 FILE *appendto_file(const wchar_t *commit);
