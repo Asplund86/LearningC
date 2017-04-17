76da8490f40f5e12875f94540f1b8c3bc4772901
Emil Asplund <emilasplund86@gmail.com>
2017-04-17 02:23:08 +0200

    Ändringar i övning 10_5

--- ovningar/10/10_5/main.c
+++ ovningar/10/10_5/main.c
@@ -4,7 +4,19 @@
 #include <string.h>
 #include <wctype.h>
 #include "elib.h"
+#define N 100
 
+//Funktion för att hitta vilken index
+//första siffran finns.
+int index_first_number(wchar_t *array){
+  int c = 0;
+  for(int i = 0; i < wcslen(array); i++){
+    if(iswdigit(array[i]))
+      break;
+    c += 1;
+  }
+  return c;
+}
 
 int main(){
 
